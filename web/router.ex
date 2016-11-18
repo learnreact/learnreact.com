c69491b7552592a672691b0279cef81d023586fd
Michael Chan <mijoch@gmail.com>
2016-11-17 20:42:13 -0800

    router: render LessonsController :index at root

--- web/router.ex
+++ web/router.ex
@@ -17,7 +17,7 @@ defmodule LearnReact.Router do
     pipe_through :browser # Use the default browser stack
     
     resources "/lessons", LessonController
-    get "/", PageController, :index
+    get "/", LessonController, :index
   end
 
   # Other scopes may use custom stacks.
