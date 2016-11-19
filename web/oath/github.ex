21701d060b62d2ae982a6965b948cf3aeef73973
Michael Chan <mijoch@gmail.com>
2016-11-18 20:54:55 -0800

    chore: setup Oauth2 groundwork

---
+++ web/oath/github.ex
@@ -0,0 +1,39 @@
+defmodule GitHub do
+  use OAuth2.Strategy
+
+  # Public API
+
+  def client do
+    OAuth2.Client.new([
+      strategy: __MODULE__,
+      client_id: System.get_env("GITHUB_CLIENT_ID"),
+      client_secret: System.get_env("GITHUB_CLIENT_SECRET"),
+      redirect_uri: "https://learnreact.com/auth/github/callback",
+      site: "https://api.github.com",
+      authorize_url: "https://github.com/login/oauth/authorize",
+      token_url: "https://github.com/login/oauth/access_token"
+    ])
+  end
+
+  def authorize_url! do
+    OAuth2.Client.authorize_url!(client(), scope: "user,public_repo")
+  end
+
+  # you can pass options to the underlying http library via `opts` parameter
+  def get_token!(params \\ [], headers \\ [], opts \\ []) do
+    OAuth2.Client.get_token!(client(), params, headers, opts)
+  end
+
+  # Strategy Callbacks
+
+  def authorize_url(client, params) do
+    OAuth2.Strategy.AuthCode.authorize_url(client, params)
+  end
+
+  def get_token(client, params, headers) do
+    client
+    |> put_param(:client_secret, client.client_secret)
+    |> put_header("accept", "application/json")
+    |> OAuth2.Strategy.AuthCode.get_token(params, headers)
+  end
+end
--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule LearnReact.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :assign_current_user
   end
 
   pipeline :api do
@@ -18,6 +19,21 @@ defmodule LearnReact.Router do
     
     resources "/lessons", LessonController
     get "/", LessonController, :index
+  end
+
+  scope "/auth", LearnReact do
+    pipe_through :browser
+
+    get "/:provider", AuthController, :index
+    get "/:provider/callback", AuthController, :callback
+    delete "/logout", AuthController, :delete
+  end
+
+  # Fetch the current user from the session and add it to `conn.assigns`. This
+  # will allow you to have access to the current user in your views with
+  # `@current_user`.
+  defp assign_current_user(conn, _) do
+    assign(conn, :current_user, get_session(conn, :current_user))
   end
 
   # Other scopes may use custom stacks.
