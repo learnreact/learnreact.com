6a897175f8830ba9ff8a34307d3b175328b8c95c
Michael Chan <mijoch@gmail.com>
2016-12-30 01:25:20 -0800

    feat: make lesson-slug configurable

--- web/models/lesson.ex
+++ web/models/lesson.ex
@@ -20,30 +20,9 @@ defmodule LearnReact.Lesson do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:title, :description, :video_embed, :thumbnail_url, :notes, :hidden, :course_id])
-    |> validate_required([:title, :description, :video_embed, :thumbnail_url, :hidden, :course_id])
-    |> slugify_title()
+    |> cast(params, [:title, :description, :video_embed, :thumbnail_url, :notes, :hidden, :course_id, :slug])
+    |> validate_required([:title, :description, :video_embed, :thumbnail_url, :hidden, :course_id, :slug])
     |> unique_constraint(:slug)
-  end
-
-  @doc """
-  Modifies the changeset to use slugify'd title.
-  """
-  defp slugify_title(changeset) do
-    if title = get_change(changeset, :title) do
-      put_change(changeset, :slug, slugify(title))
-    else
-      changeset
-    end
-  end
-
-  @doc """
-  Returns title slug for friendly URLs
-  """
-  defp slugify(str) do
-    alias LearnReact.Inflectors
-
-    Inflectors.parameterize(str)
   end
 end
 
