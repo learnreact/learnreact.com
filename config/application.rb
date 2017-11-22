8903323c338afbfb633030b0168eea9fc922e7d4
Michael Chan <mijoch@gmail.com>
2017-11-21 19:49:43 -0800

    Revert "try: add changelog via rack_reverse_proxy again"
    This reverts commit 77ea127df1e0f4d8368a49e31820ed2f8ea77c2b.

--- config/application.rb
+++ config/application.rb
@@ -17,11 +17,10 @@ module LearnReact
     # -- all .rb files in that directory are automatically loaded.
 
     config.middleware.insert(0, Rack::ReverseProxy) do
-      reverse_proxy_options preserve_host: false
-      if Rails.env.production? or Rails.env.staging?
-        reverse_proxy_options force_ssl: true, replace_response_host: true
+      if Rails.env.production?
+        reverse_proxy_options preserve_host: true
+        reverse_proxy '/changelog', 'https://learnreact.github.io/changelog/index.html'
       end
-      reverse_proxy /^\/changelog(\/?.*)$/, 'https://learnreact.github.io/changelog/'
     end
   end
 end
