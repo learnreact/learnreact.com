b37762f1ac797194b9d85e8c0ed39fca68b1cbc0
Michael Chan <mijoch@gmail.com>
2018-01-02 14:20:03 -0800

    LOL

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def new
-    rediruct_to Rails.env.production? ? "https://learnreact.com/auth/github" : "/auth/github"
+    redirect_to Rails.env.production? ? "https://learnreact.com/auth/github" : "/auth/github"
   end
 
   def create
