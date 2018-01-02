1e920e48095c6a2819c32b26472c4750ca4dc7bf
Michael Chan <mijoch@gmail.com>
2018-01-02 14:34:16 -0800

    🛠: omniAuth.config.full_host in production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], 
 end
+
+if Rails.env.production? OmniAuth.config.full_host = "https://learnreact.com"
