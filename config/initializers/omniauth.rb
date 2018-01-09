70ce543b9c191b37b4bf5a841214a7a586f4c160
Michael Chan <mijoch@gmail.com>
2018-01-08 22:39:30 -0800

    Revert "fixup! 🛠: omniAuth.config.full_host in production"
    This reverts commit d62fa9451d3755771a73e6ef53fcf2b1a1f7711b.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], 
 end
 
-OmniAuth.config.full_host = "https://learnreact.com" if Rails.env.production? 
+if Rails.env.production? OmniAuth.config.full_host = "https://learnreact.com"
