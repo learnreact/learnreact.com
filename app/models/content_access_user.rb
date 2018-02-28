a19f95de6723923bec89fbdd7440062e3b4be855
Michael Chan <mijoch@gmail.com>
2018-02-28 08:34:38 -0800

    add subscribe buttons where necessary

--- app/models/content_access_user.rb
+++ app/models/content_access_user.rb
@@ -1,4 +1,12 @@
 class ContentAccessUser < SimpleDelegator
+  def has_subscription?
+    if self.present? && self.subscription.present?
+      return true
+    else
+      return false
+    end
+  end
+
   def can_access_course?(course)
     if self.present? && (self.subscription.present? || self.courses.include?(course))
       return true
